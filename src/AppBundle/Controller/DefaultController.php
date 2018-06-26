740cc105b17e257d8d03e2e8989b7f1cf6f6775b
Charlybs <carlos11811@gmail.com>
2018-06-26 17:18:06 +0200

    search users

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Abraham\TwitterOAuth\TwitterOAuth;
 
 class DefaultController extends Controller
 {
@@ -13,14 +16,19 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        
+        
+        
+        
+        
+        return $this->render('default/index.html.twig',
+                array(
+                        'contenido' => null
+                    ));
     }
     
     /**
-     * @Route("/about", name="About")
+     * @Route("/about", name="about")
      */
     public function aboutAction(Request $request)
     {
@@ -28,5 +36,44 @@ class DefaultController extends Controller
         return $this->render('default/about.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+    /**
+     * @Route("/search", name="search")
+     */
+    public function searchAction(Request $request)
+    {
+        
+        $form = $this->createFormBuilder()
+            ->add('user', TextType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $query = $form->getData();
+
+            $connection = new TwitterOAuth(
+                $this->getParameter('twitter_consumer_api_key'),
+                $this->getParameter('twitter_consumer_api_key_secret'),
+                $this->getParameter('twitter_access_token'),
+                $this->getParameter('twitter_access_token_secret')
+                );
+        
+        //Only the first 1,000 matching results are available.
+        $content = $connection->get("users/search", ["q" => $query]);
+        
+        return $this->render('default/search.html.twig',
+                array(
+                        'contenido' => $content,
+                        'form' => $form->createView()
+                    ));
+        }
+        
+        return $this->render('default/search.html.twig',
+                array(
+                    'contenido' => null,
+                        'form' => $form->createView()
+                    ));
     }
 }
